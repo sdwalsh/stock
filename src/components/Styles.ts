4fc013b2dc06e10b628841b0d2e91600e0a7fb8a
Sean Walsh <sdwalsh@mirango.io>
2018-11-19 10:32:58 -0500

    add styles consts

---
+++ src/components/Styles.ts
@@ -0,0 +1,20 @@
+/*
+  CSS Styles
+
+  Using CSS Grid
+  https://caniuse.com/#feat=css-grid
+  ~88.3% (firefox, chrome, safari, edge)
+*/
+
+const GridCSS: React.CSSProperties= {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(5, 1fr)',
+}
+
+const GridRow: React.CSSProperties = {
+
+}
+
+const InventoryItem: React.CSSProperties = {
+  margin: '10px',  
+}
