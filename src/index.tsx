e7024bd3a55337cf6ce372ece5291076a9d24451
Sean Walsh <sdwalsh@mirango.io>
2018-11-19 12:42:02 -0500

    add css imports for @blueprintjs

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
 import App from './App';
-import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
 
 ReactDOM.render(
   <App />,
