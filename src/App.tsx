030ade59cbfdb546d20b8ba8f9c3fdbb9bc18699
Sean Walsh <sdwalsh@mirango.io>
2018-11-19 12:40:46 -0500

    add @blueprintjs/core

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './App.css';
+import { Grid } from './components/Grid';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <Grid x={5} y={5} />
       </div>
     );
   }
